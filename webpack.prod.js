337a92001f34fb5cb9963f3d30f0ced7d584c989
Sam Ageloff <sam@getambassador.com>
2017-12-11 11:35:34 -0500

    adding devServer to prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
@@ -6,6 +7,10 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: process.env.PORT || 3000
+  },
   plugins: [
     new UglifyJSPlugin({
       sourceMap: true
