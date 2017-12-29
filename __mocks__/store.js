f361b70d88533425e085aff54821e8ca0d688782
Sam Ageloff <sam@getambassador.com>
2017-12-29 13:42:37 -0500

    adjusting

---
+++ __mocks__/store.js
@@ -0,0 +1,126 @@
+data: {
+  workouts: {
+    browse: [
+      {
+        title: 'Browse by muscle group',
+        collection: [
+          {
+            id: 1,
+            name: 'Abs & Core',
+            list: [
+              {
+                id: 1,
+                title: 'Core Crunch 2.0',
+                level: 'Intermediate',
+                equipment: 'Basic Equipment',
+                routine: [
+                  {
+                    section: 'Warm-Up',
+                    exercises: [1, 2]
+                  },
+                  {
+                    section: 'Core Conditioning',
+                    exercises: [3, 4]
+                  },
+                  {
+                    section: 'Cool down',
+                    exercises: [5]
+                  }
+                ]
+              },
+              {
+                id: 2,
+                title: 'Ab Burner 2.0',
+                level: 'Beginner',
+                equipment: 'No Equipment',
+                routine: [
+                  {
+                    section: 'Another sub section',
+                    exercises: [1, 2]
+                  },
+                  {
+                    section: 'Stretch',
+                    exercises: [3, 4, 5]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            id: 2,
+            name: 'Arms & Shoulders',
+            list: [
+              {
+                id: 4,
+                title: 'Abs and arms',
+                time: 1020,
+                level: 'Intermediate',
+                equipment: 'No Equipment'
+              },
+              {
+                id: 5,
+                title: 'Push & Pull',
+                time: 900,
+                level: 'Beginner',
+                equipment: 'No Equipment'
+              },
+              {
+                id: 6,
+                title: 'Boxer Arms',
+                time: 960,
+                level: 'Advanced',
+                equipment: 'No Equipment'
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    exercises: [
+      {
+        1: {
+          title: 'High Plank Arm Reaches',
+          subtitle: 'Alternating',
+          length: 60,
+          thumbnail: '',
+          animated: '',
+        }
+      },
+      {
+        2: {
+          title: 'Kneeling Med Ball Lifts',
+          subtitle: 'Left Side',
+          length: 30,
+          thumbnail: '',
+          animated: ''
+        }
+      },
+      {
+        3: {
+          title: 'Push Up to Side Plank',
+          subtitle: 'Right Side',
+          length: 30,
+          thumbnail: '',
+          animated: ''
+        }
+      },
+      {
+        4: {
+          title: 'Boat to Low Boat',
+          subtitle: '',
+          length: 30,
+          thumbnail: '',
+          animated: ''
+        }
+      },
+      {
+        5: {
+          title: 'Hip Lift March',
+          subtitle: 'Alternating',
+          length: 60,
+          thumbnail: '',
+          animated: ''
+        }
+      }
+    ]
+}
