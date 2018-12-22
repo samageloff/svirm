63118911c9ef209467c7ac222d85be580b89297c
Sam Ageloff <samuelageloff@gmail.com>
2018-12-22 06:37:58 -0500

    wiring up squares

--- src/components/julio-le-parc/components/BigSquare/style.js
+++ src/components/julio-le-parc/components/BigSquare/style.js
@@ -1,7 +1,7 @@
 import { Map } from 'immutable'
 import { palatte } from 'julio/styles/colors'
 
-const bigSquare = flip => Map({
+export const bigSquareStyle = flip => Map({
   opacity: '.85',
   boxShadow: `0 0 0 .5px ${palatte.lines}`,
   flexWrap: 'wrap',
