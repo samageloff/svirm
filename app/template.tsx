fd43d23d8d3e70ef4ce9ea6b61f2ab790bcf24bb
Sam <samuelageloff@gmail.com>
2025-04-13 12:47:00 -0500

    remove transition for now

--- app/template.tsx
+++ app/template.tsx
@@ -5,14 +5,7 @@ import { AnimatePresence, motion } from "motion/react";
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <AnimatePresence>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.35, ease: "easeInOut" }}
-      >
-        {children}
-      </motion.div>
+      <motion.div>{children}</motion.div>
     </AnimatePresence>
   );
 }
